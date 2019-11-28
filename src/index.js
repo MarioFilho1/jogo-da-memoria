aabea2019249b77b0a3f1393d3d0e54d8ef46b65
mario filho <mariosantofilho@gmail.com>
2019-11-27 22:10:49 -0300

    Desafio avançado aula 022

--- src/index.js
+++ src/index.js
@@ -1,118 +1,45 @@
 const $root = document.querySelector("#root");
 const $containerPlay = document.createElement("div");
-const $memoryCard1 = document.createElement("article");
 const $memoryCardFront = document.createElement("article");
-const $memoryCard2 = document.createElement("article");
-const $memoryCard3 = document.createElement("article");
-const $memoryCard4 = document.createElement("article");
-const $memoryCard5 = document.createElement("article");
-const $memoryCard6 = document.createElement("article");
-const $memoryCard7 = document.createElement("article");
-const $memoryCard8 = document.createElement("article");
-const $memoryCard9 = document.createElement("article");
-const $icon = `
-  <img
-  class='icon' 
-  src='/img/icon-collabcode.png' 
-  alt='gueio mascote da Collab Code'
-  />`;
+var $memoryCard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+var $icon = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-const $iconc = `
+$memoryCard.forEach(function(index) {
+  $memoryCard[index] = document.createElement("article");
+});
+
+// -----------
+const $iconC = `
   <img
   class='icon -circle'
   src='/img/icon-c.png'
   alt='icon livro linguagem C'
   />
   `;
-const $icon2 = `
+$icon.forEach(function(index) {
+  $icon[index] = `
   <img
-  class='icon' 
-  src='/img/icon-collabcode.png' 
-  alt='gueio mascote da Collab Code'
-  />`;
-const $icon3 = `
-  <img
-  class='icon' 
-  src='/img/icon-collabcode.png' 
-  alt='gueio mascote da Collab Code'
-  />`;
-const $icon4 = `
-  <img
-  class='icon' 
-  src='/img/icon-collabcode.png' 
-  alt='gueio mascote da Collab Code'
-  />`;
-const $icon5 = `
-  <img
-  class='icon' 
-  src='/img/icon-collabcode.png' 
-  alt='gueio mascote da Collab Code'
-  />`;
-const $icon6 = `
-  <img
-  class='icon' 
-  src='/img/icon-collabcode.png' 
-  alt='gueio mascote da Collab Code'
-  />`;
-const $icon7 = `
-  <img
-  class='icon' 
-  src='/img/icon-collabcode.png' 
-  alt='gueio mascote da Collab Code'
-  />`;
-const $icon8 = `
-  <img
-  class='icon' 
-  src='/img/icon-collabcode.png' 
-  alt='gueio mascote da Collab Code'
-  />`;
-const $icon9 = `
-  <img
-  class='icon' 
-  src='/img/icon-collabcode.png' 
-  alt='gueio mascote da Collab Code'
-  />`;
+   class='icon'
+   src='/img/icon-collabcode.png'
+   alt='gueio mascote da Collab Code'
+   />
+   `;
+});
+
+// ------------
 $root.insertBefore($containerPlay, null);
 $containerPlay.classList.add("container-play");
 
-//-------------
-$containerPlay.insertBefore($memoryCard1, null);
-$memoryCard1.classList.add("memory-card");
-$memoryCard1.insertAdjacentHTML("afterBegin", $icon);
 // ------------
-$containerPlay.insertBefore($memoryCardFront, $memoryCard1);
+$memoryCard.forEach(function(index, key) {
+  $containerPlay.insertBefore($memoryCard[key], null);
+  $memoryCard[key].classList.add("memory-card");
+  $memoryCard[key].insertAdjacentHTML("afterBegin", $icon[key]);
+  console.log(index, key);
+});
+
+//-------------
+$containerPlay.insertBefore($memoryCardFront, $memoryCard[0]);
 $memoryCardFront.classList.add("memory-card");
 $memoryCardFront.classList.add("-front");
-$memoryCardFront.insertAdjacentHTML("afterBegin", $iconc);
-//-------------
-$containerPlay.insertBefore($memoryCard2, null);
-$memoryCard2.classList.add("memory-card");
-$memoryCard2.insertAdjacentHTML("afterBegin", $icon2);
-//-------------
-$containerPlay.insertBefore($memoryCard3, null);
-$memoryCard3.classList.add("memory-card");
-$memoryCard3.insertAdjacentHTML("afterBegin", $icon3);
-//-------------
-$containerPlay.insertBefore($memoryCard4, null);
-$memoryCard4.classList.add("memory-card");
-$memoryCard4.insertAdjacentHTML("afterBegin", $icon4);
-//-------------
-$containerPlay.insertBefore($memoryCard5, null);
-$memoryCard5.classList.add("memory-card");
-$memoryCard5.insertAdjacentHTML("afterBegin", $icon5);
-//-------------
-$containerPlay.insertBefore($memoryCard6, null);
-$memoryCard6.classList.add("memory-card");
-$memoryCard6.insertAdjacentHTML("afterBegin", $icon6);
-//-------------
-$containerPlay.insertBefore($memoryCard7, null);
-$memoryCard7.classList.add("memory-card");
-$memoryCard7.insertAdjacentHTML("afterBegin", $icon7);
-//-------------
-$containerPlay.insertBefore($memoryCard8, null);
-$memoryCard8.classList.add("memory-card");
-$memoryCard8.insertAdjacentHTML("afterBegin", $icon8);
-//-------------
-$containerPlay.insertBefore($memoryCard9, null);
-$memoryCard9.classList.add("memory-card");
-$memoryCard9.insertAdjacentHTML("afterBegin", $icon9);
+$memoryCardFront.insertAdjacentHTML("afterBegin", $iconC);
